a16c92e845ea7d4fb0950bb895d6186732eac62e
FloraSoledad <vargas.flora@hotmail.com>
2022-08-02 04:13:19 -0300

    archivos actualizado y terminado

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = 3030;
+const mainRouter = require('./routers/main');
+
+
+
+app.use(express.static('public'));
+
+/* rutas */
+app.use('/', mainRouter);
+
+
+app.listen(3030, () => console.log(`server running in http://localhost:${port}`))
